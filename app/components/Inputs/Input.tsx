69a4b49541a8459afe44b450c4c6bc1044c142ce
unknown <vikcanada90@gmail.com>
2024-02-20 02:45:03 -0700

    Added error focus

--- app/components/Inputs/Input.tsx
+++ app/components/Inputs/Input.tsx
@@ -64,6 +64,7 @@ const Input: FC<InputProps> = ({
         peer-placeholder-shown:translate-y-0
         peer-focus:scale-75
         peer-focus:-translate-y-4
+        ${errors[id] ? "text-rose-500" : "text-zinc-400"}
         `}
       >
         {label}
